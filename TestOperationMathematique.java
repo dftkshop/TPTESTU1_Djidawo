53c757c79b78f661ca4ad8ad72af3eaae2738c2c
Djidawokeylian <kdjidawo@gmail.com>
2025-11-12 12:16:46 +0100

    ajout des tests sur trier et maxNumber

--- TestOperationMathematique.java
+++ TestOperationMathematique.java
@@ -1,4 +1,5 @@
 
+import static jdk.internal.lang.stable.StableUtil.array;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +15,18 @@ public class TestOperationMathematique {
     void testFactoriel() {
         assertEquals(120, op.factoriel(5));
         assertEquals(1, op.factoriel(0));
+    }
+    @Test
+    void testTrier(){
+        Integer[] liste = {1,2,4,57,4,1,5};
+        assertArrayEquals(liste, op.trier(liste));
+
+    }
+    @Test
+    void testMax(){
+        double[] liste = {1,2,4,57,4,1,5};
+        assertArrayEquals(liste, new double[]{op.maxNumba(liste)});
+
     }
 
 
